220cab31361fce22ee24879f2f50a4dfb7e92bb7
박병관 <85085375+codingbotPark@users.noreply.github.com>
2021-10-21 13:27:48 +0900

    10.14, 10.17~10.23

---
+++ data struct class (10.14).py
@@ -0,0 +1,17 @@
+# 10.14
+# 윈도우 창 만들기
+# from tkinter import *
+# w = Tk()
+# w.title('PARK BYOUNG KWAN')
+# w.geometry('1000x200')#소문자x사용
+# w.mainloop()
+
+# print('1:', 10 > 5)
+# print('2:', 10 < 3)
+# print('3:', 10 > 5 or 10 < 3)
+# print('4:',10 > 5 and 10 < 3)
+# print('5:', not True)
+# print('6:',not True and True)
+# print('7',not True or True)
+
+# ctrl + alt + l = 코드정렬
