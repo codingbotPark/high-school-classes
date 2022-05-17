cd769545973cbeac0d265eae8e9cee18b063ad71
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-05-17 10:51:04 +0900

    refactoring
    cd.. 의 로직을 [\ 기준으로 나눈 후 마지막 경로 빼기] -> [뒤에서부터 \가 나올 때 까지 없애기] 로 변경

--- java practice/src/kr/hs/dgsw/java/dept23/task1/CommandCd.java
+++ java practice/src/kr/hs/dgsw/java/dept23/task1/CommandCd.java
@@ -10,43 +10,58 @@ public class CommandCd extends AbstractCommand {
 	public CommandCd(File currentDirectory, String commandLine) {
 		super(currentDirectory, commandLine);
 	}
+	
+	// cd .. 했을 때
+	public File goToBack() {
+		StringBuffer currentDirectoryPath = new StringBuffer(currentDirectory.getAbsolutePath());
+		for (int i = currentDirectoryPath.length()-1;i>0;i--) {
+			if(currentDirectoryPath.charAt(i) == '\\') {
+				// 안 되면 슬레쉬 없애기
+				break;
+			} else {
+				currentDirectoryPath.deleteCharAt(i);
+			}
+		}
+		
+		// StringBuffer를 String으로 바꾸고 File로 변환시켜서 return
+		String temp = new String(currentDirectoryPath);
+		File dir = new File(temp);
+		
+		return dir;
+	}
+	
+	// cd로 안으로 갈 때
+	public File goToIn(String path) {
+		File dir = new File(currentDirectory + "\\" + path);
+		if (dir.exists() && dir.isDirectory()) {
+			return dir;
+		} else {
+			System.out.println("cannot find the path");
+			// 없는 디렉토리면 현제 디렉토리를 리턴
+			return currentDirectory;	
+		}
+	}
+
 
 	
 	@Override
 	public File executeCommand() {
+		// 기본적인 return 은 현재
 		File returnDir = currentDirectory;
 		
-		// TODO
-		String[] commandArr = commandLine.split(" ", 2);
-		
-		// 이전으로
-		if (commandArr[1].equals("..")) {
-			
-			
-			// window는 \\\\ 로 나눠야 한다
-			String[] tempLocationArr = String.valueOf(currentDirectory).split("\\\\");
-			
-			String temp = "";
-			// 현재있는 위치의 마지막 위치를 뺀다
-			for (int i = 0;i < tempLocationArr.length - 1;i++) {
-				temp = temp  + tempLocationArr[i] + "\\";
-			}	
-			File dir = new File(temp);
-			if (dir.exists()) {
-				returnDir = dir;	
-			} else {
-				System.out.println("cannot find the path");
-			}
-			
-		} else { // 안으로
-			File dir = new File(currentDirectory + "\\" + commandArr[1]);
-			if (dir.exists() && dir.isDirectory()) {
-				returnDir = dir;
-			} else {
-				System.out.println("cannot find the path");
+		// cd 다음 명령어가 들어왔는지(cd가 2글자여서 2)
+		if (commandLine.indexOf(" ",2) != -1) {
+			//commandLine을 [명령어] [추가정보] 로 나눔
+			String[] commandArr = commandLine.split(" ",2);
+			if (commandArr[1].equals("..")) {
+				returnDir = goToBack();
+			} 
+			else{
+				returnDir = goToIn(commandArr[1]);
 			}
 		}
-
+		
+		// TODO
 		return returnDir;
 
 	}
