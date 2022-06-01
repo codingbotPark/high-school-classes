ec27a25638e79cfee62a3ef196ca47a48bd05b36
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-06-01 15:57:58 +0900

    fin task

--- 220531/main.c
+++ 220531/main.c
@@ -40,37 +40,37 @@ void USART_Transmitter(unsigned char txData){
 	UDR1 = txData;
 }
 
-// 인터럽트로 들어온 값을 받기
-int number = 0;
+//// 인터럽트로 들어온 값을 받기
+int number = -1; // ++ 을 먼저하기 때문에 기본값을 -1로 해준다
+int flag = 0; // 위인지 아래인지
+
 ISR(USART1_RX_vect){
 	if(UCSR1A & (1<<RXC1)){
-		//rxData = UDR1;//수신 버퍼 데이터를 변수에 저장
-		
-		rxData = UDR1;
+		rxData = UDR1;//수신 버퍼 데이터를 변수에 저장
 		USART_Transmitter(rxData);
 
-		//SendText(DDRAM_ADDR | 0x00, rxData);
-		//SendText(DDRAM_ADDR | 0x00, rxData);
-		
-		char * str;
-		str = rxData;
-		
-		//for (int i = 0;i;)
-		if (rxData == '\n'){
-			//USART_Transmitter()
-			USART_Transmitter(number + '0');
-			number = 0;
-		} else {
-			number++;
-			//SendData(rxData);
+		if (rxData != '\n'){
 			
-			//SendText(DDRAM_ADDR | 0x00, &str);
+			number++;
+			USART_Transmitter(number+'0');
+			
+			if (number == 16){ // 한 줄의 끝일 때
+				// number가 16일 때 두 번째 라인으로 보낸다
+				if (flag){
+
+					SendCommand(CMD_CLEAR_DISPLAY);
+					_delay_ms(50);
+					SendCommand(DDRAM_ADDR | 0x00);	
+					flag = 0;
+				} else {
+					SendCommand(DDRAM_ADDR | 0x40);	
+					flag = 1;
+				}
+				number = 0;
+			}
+			
+			SendData(rxData); // 데이터를 lcd에 보낸다
 		}
-		
-		
-		
-		//SendText(DDRAM_ADDR | 0x00,"HELLO"); // 1행에 문자 출력
-		//SendText(DDRAM_ADDR | 0x40, "1234567890"); // 2행에 문자 출력
 	}
 }
 
