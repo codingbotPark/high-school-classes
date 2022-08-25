723aca8e4e6703b1c6fc70886513c657ad567b7e
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-08-25 12:16:04 +0900

    c#으로 글씨 찍기, 테트리스 블록 출력, 기본적인 블록 내려오기, 맵 출력, 맵 탈출 방지, 블록 이동

--- delay_clear/Program.cs
+++ delay_clear/Program.cs
@@ -21,6 +21,32 @@ namespace ConsoleApp2
 
         static byte[,] num_byte_2 = new byte[2, 8] { { 0x00, 0x38, 0x44, 0x04, 0x08, 0x10, 0x20, 0x7c }, { 0x00, 0x38, 0x44, 0x04, 0x18, 0x04, 0x44, 0x38 } };
 
+        static byte[,] background = new byte[22, 12]
+        {
+            {1,1,1,1,1,1,1,1,1,1,1,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,0,0,0,0,0,0,0,0,0,0,1 },
+            {1,1,1,1,1,1,1,1,1,1,1,1 },
+        };
+
         static byte[,] num_byte_3 = new byte[10, 8] {
             { 0x00,0x38,0x44,0x4c,0x54,0x64,0x44,0x38}, // 0
                 {0x00,0x10,0x30,0x50,0x10,0x10,0x10,0x7c}, // 1
@@ -43,8 +69,10 @@ namespace ConsoleApp2
             {0,0,0,0 },
         };
 
-        static int x = 0;
-        static int y = 0;
+        static int x = 5;
+        static int y = 5;
+
+        static int count = 0;
 
         // static은 객체화 하지 않아도 바로 메모리에 올라간다
         static void Main(string[] args)
@@ -71,30 +99,95 @@ namespace ConsoleApp2
             ConsoleKeyInfo key_value;
             String ch;
 
+
+            makeBackGround();
+
             while (true)
             {
+
                 if (Console.KeyAvailable)
                 {   
-                    key_value = Console.ReadKey();
+                    // true를 줘서 키보드 입력이 출력되지 않게 한다
+                    key_value = Console.ReadKey(true);
                     ch = key_value.Key.ToString();
 
-                    if (ch == "a")
+                    // 블록 이동
+                    if (ch == "A") // 왼쪽
                     {
-                        Console.WriteLine("a");
+                        if (overlap_check(-1,0) == 0)
+                        {
+                            deleteBlock();
+                            x--;
+                            makeBlock();
+                        }
+                        
                     } 
-                    else if(ch == "s")
+                    else if(ch == "S") // 아래
                     {
-                        Console.WriteLine("s");
+                        if (overlap_check(0, 1) == 0)
+                        {
+
+                            deleteBlock();
+                            y++;
+                            makeBlock();
+                        }
+
+                    }
+                    else if(ch == "D") // 오른쪽
+                    {
+                        if (overlap_check(1, 0) == 0)
+                        {
+                            deleteBlock();
+                            x++;
+                            makeBlock();
+                        }
+
                     }
                 }
-                //makeBlock();
-                //Thread.Sleep(1000);
-                //deleteBlock();
-                //y++;
+
+                if (count > 100)
+                {
+                    count = 0;
+
+                    if (overlap_check(0, 1) == 0)
+                    {
+                        deleteBlock();
+                        y++;
+                        makeBlock();
+                    }
+                }
+
+                Thread.Sleep(10);
+                count++;
 
             }
 
-            static void makeBlock()
+            void makeBackGround()
+            {
+
+                int x_pos = 0;
+                int y_pos = 0;
+
+                for (int j = 0; j < 22; j++)
+                {
+                    for (int i = 0; i < 12; i++)
+                    {
+                        if (background[j, i] == 1)
+                        {
+                            Console.SetCursorPosition(i + x_pos, j + y_pos);
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.SetCursorPosition(i + x_pos, j + y_pos);
+                            Console.Write("-");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            void makeBlock()
             {
                 for (int j = 0; j < 4; j++)
                 {
@@ -104,21 +197,16 @@ namespace ConsoleApp2
                         {
                             // setCursorPointer는 c에서의 gotoXY와 같다
                             // 기준은 0,0이 왼쪽 위이다
-                            Console.SetCursorPosition(i, j + y);
+                            Console.SetCursorPosition(i + x, j + y);
                             Console.Write("*");
 
-                        }
-                        else
-                        {
-                            Console.SetCursorPosition(i, j + y);
-                            Console.Write("-");
-                        }
+                        } // 원래 else가 있었는데, background가 출력되고 출력을 하기 때문에 그냥 else일 때는 탈출
                     }
                     Console.WriteLine();
                 }
             }
 
-                static void deleteBlock()
+                void deleteBlock()
                 {
                     for (int j = 0; j < 4; j++)
                     {
@@ -126,18 +214,30 @@ namespace ConsoleApp2
                         {
                             if (block_L[j, i] == 1)
                             {
-                                Console.SetCursorPosition(i, j + y);
-                                Console.Write(" ");
-                            }
-                            else
-                            {
-                                Console.SetCursorPosition(i, j + y);
-                                Console.Write(" ");
+                                Console.SetCursorPosition(i+x, j + y);
+                                Console.Write("-");
                             }
                         }
-                        Console.WriteLine();
                     }
                 }
+
+            int overlap_check(int tmp_x, int tmp_y)
+            {
+                int overlap_count = 0;
+
+                for (int j = 0; j < 4; j++)
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if (block_L[j,i] == 1 && background[j +y+tmp_y, i+x+tmp_x] == 1)
+                        {
+                            overlap_count++;
+                        }
+                    }
+                }
+
+                return overlap_count;
+            }
 
 
             //byte num = 0x23;
