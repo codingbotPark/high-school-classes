d3a33b83e57c2b6f64b0d8105f0886347f4bb106
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-10-30 22:22:18 +0900

    [UPDATE] rightSorting

--- 수행평가1/server/src/main/java/com/example/server/util/Comparator.java
+++ 수행평가1/server/src/main/java/com/example/server/util/Comparator.java
@@ -9,9 +9,9 @@ public class Comparator implements java.util.Comparator<LoadPost> {
     @Override
     public int compare(LoadPost o1, LoadPost o2) {
         if (o1.getTime().isAfter(o2.getTime())) {
-            return 1;
-        } else if (o1.getTime().isBefore(o2.getTime())) {
             return -1;
+        } else if (o1.getTime().isBefore(o2.getTime())) {
+            return 1;
         }
         return 0;
     }
