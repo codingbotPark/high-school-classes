429acc5ec535dbc28e1c3560c7084d6f03c3b89b
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-11-29 19:24:43 +0900

    [FIN]

--- 221129 수행2/arduino/arduino.ino
+++ 221129 수행2/arduino/arduino.ino
@@ -1,29 +1,26 @@
 #include <Servo.h>
-int servoPin = 3;
 Servo servo;
+int servoPin = 10;
 int angle = 0;
-
 void setup() {
   // put your setup code here, to run once:
   Serial.setTimeout(100);
   Serial.begin(9600);
   pinMode(5,OUTPUT); // led1
   pinMode(6,OUTPUT); // led2
+  pinMode(servoPin,OUTPUT);
 
   servo.attach(servoPin);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-      digitalWrite(5,HIGH);
-      digitalWrite(6,HIGH);
-    servo.write(10); 
-   
+  // servo.write(180);
 
 
   if (Serial.available()){
     String getData = Serial.readString();
-    
+    Serial.println(getData);
     if (getData == "1on") { // led1 on
       digitalWrite(5,HIGH);
     } else if (getData == "2on"){
@@ -31,8 +28,13 @@ void loop() {
     } else if (getData == "1off"){
       digitalWrite(5,LOW);
     } else if(getData == "2off"){
-      digitalWrite(5,LOW);
+      digitalWrite(6,LOW);
+    } else if (getData == "3left"){
+      servo.write(10);
+    } else if (getData == "3right"){
+      servo.write(170);
     }
 
+    Serial.flush();
   }
 }
