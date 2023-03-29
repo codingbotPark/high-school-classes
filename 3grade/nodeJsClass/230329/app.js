f405ecbda7f8e16ebc19a605f83ebe0d00ac7ae9
박병관 <85085375+codingbotPark@users.noreply.github.com>
2023-03-29 16:13:04 +0900

    [STUDY] middleware

--- 3grade/nodeJsClass/230329/app.js
+++ 3grade/nodeJsClass/230329/app.js
@@ -1,6 +1,17 @@
 const express = require("express")
+const morgan = require("morgan")
+const path = require('path')
 
 const app = express()
+
+app.use(morgan('dev'))
+// 이미지 형식으로 요구시 public에서 이미지를 찾는다
+app.use(express.static(path.join(__dirname, 'public')))
+// 객체가 오면 json으로 만든다
+app.use(express.json())
+// 
+app.use(express.urlencoded({extended:false}))
+
 app.set('port',process.env.PORT || 3000)
 
 app.get('/',(req,res) => {
