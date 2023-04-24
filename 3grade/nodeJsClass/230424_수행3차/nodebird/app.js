f40bae33692d215886c730cd0b67d46b7bb33bc6
박병관 <85085375+codingbotPark@users.noreply.github.com>
2023-04-24 09:58:33 +0900

    [INIT] nodebird

---
+++ 3grade/nodeJsClass/230424_수행3차/nodebird/app.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const cookieParser = require("cookie-parser")
+const morgan = require("morgan")
+const path = require("path")
+const session = require("express-session")
+const nunjucks = require("nunjucks")
+const dotenv = require("dotenv")
+
+dotenv.config()
