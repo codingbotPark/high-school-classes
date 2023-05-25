8d8977507c02663b01f0e1de120a67fa2668e19b
박병관 <pbk575@gmail.com>
2023-05-25 15:58:12 +0900

    [FIN] nodebird

--- 3grade/nodeJsClass/230424_수행3차/nodebird/controllers/post.js
+++ 3grade/nodeJsClass/230424_수행3차/nodebird/controllers/post.js
@@ -1,3 +1,4 @@
+const { Op } = require("sequelize");
 const {Post, Hashtag} = require("../models")
 
 exports.afterUploadImage = (req,res) => {
@@ -32,11 +33,46 @@ exports.uploadPost = async(req,res,next) => {
 }
 
 exports.updatePost = async(req,res,next) => {
-    try{
-        const post = await Post.update({
+    try{    
+        const post = await Post.findOne({where:{id:req.params.postid}})
+        console.log("------------");
+        const prevHashtags = post.content.match(/#[^\s#]*/g)
+        const prevHashtagsWithoutShop = prevHashtags.map(str => str.slice(1))
+        console.log("prevHashtagsWithoutShop",prevHashtagsWithoutShop)
+        const prevHashTagIds = await Hashtag.findAll({
+            attributes:['id'],
+            where:{
+            title:{[Op.in]:prevHashtagsWithoutShop}
+        }})
+        // console.log(prevHashTagIds);
+        await post.removeHashtags(prevHashTagIds)
+        console.log("------------");
+
+        // const prevHashTags = await Hashtag.findAll({where:{
+        //     title:{[Op.in]:}
+        // }})
+        // if (prevHashTags){
+        //     await post.removeHashtags(prevHashTags.map(r => r))
+        // }
+
+        // 내용 수정
+        const updateResult = await Post.update({
             content:req.body.content
         },{where:{id:req.params.postid}})
-        res.send({post})
+
+        const hashtags = req.body.content.match(/#[^\s#]*/g)
+        // 수정된 내용을 기반으로 해시테그 추가
+        if (hashtags) {
+            const result = await Promise.all(
+                hashtags.map(tag => {
+                    return Hashtag.findOrCreate({where:{title:tag.slice(1).toLowerCase()}})
+                })
+            )
+            console.log(result.map(r => r[0]))
+            await post.addHashtags(result.map(r => r[0]))
+        }
+
+        res.send({updateResult})
         // res.redirect("/")
     } catch (error){
         console.error(error)
--- 3grade/nodeJsClass/230424_수행3차/nodebird/middlewares/index.js
+++ 3grade/nodeJsClass/230424_수행3차/nodebird/middlewares/index.js
@@ -2,7 +2,9 @@ exports.isLoggedIn = (req,res,next) => {
     if (req.isAuthenticated()){
         next();
     } else {
-        res.status(403).send("로그인 필요");
+        // res.status(403).redirect(`/`)
+        // res.status(403).send("로그인 필요");
+        res.write("<script>window.location=\"/\";alert('need login')</script>");
     }
 }
 
